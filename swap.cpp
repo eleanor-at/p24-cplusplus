30419ab67942880e3711731928f6d2bdeaac975e
”Eleanor <eleanor.attali@etu.minesparis.psl.eu>
2024-11-12 12:14:42 +0100

    chg

---
+++ swap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+void swap(int * pa, int * pb) 
+{
+    int t =(*pa);
+    *pa = *pb;
+    *pb = t;
+    
+}
+int main() 
+{
+    int a = 82;
+    int b = -72;
+    int * pa = &a;
+    int * pb = &b;
+    swap(pa,pb);
+    std:: cout << a << ' ' << b << std::endl;
+    return 0;
+}
