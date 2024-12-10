06df7081210d0f4389f13a430153dedf0212d51b
”Eleanor <eleanor.attali@etu.minesparis.psl.eu>
2024-12-10 14:34:12 +0100

    maj

--- rational.cpp
+++ rational.cpp
@@ -5,29 +5,23 @@ class rational
         int num;
         int denom;
     public:
-    
-    rational(int p = 0 , int q = 1): num(p), denom(q) { if (q==0) {throw 1;}} ;// syntaxe initialisation
-    class rational{friend rational operator + (rational, rational)};
-
-    void print()
-    {
+        rational(int p = 0 , int q = 1): num{p}, denom{q} { if (q==0) {throw 1;}} // syntaxe initialisation 
+        friend rational operator+(const rational &, const rational &); //évite de recopier la valeur, const évite de modifier la valeur par erreur
+       
+        void print() const //je peux appeler print sur des objets constants, on le met sur les méthodes qui ne modifient pas l'objet
+        {
         std::cout<<num<< '/' << denom << std::endl ; //void fait que la fonction execute la tache et ne renvoie rien, ici elle execute une tache qui est de renvoyer une valeur
-    }
+        }
 
-    float eval(rational r)
-    {
-        return rational(static_cast<float>(num)/denom);
-    }
-
-    rational operator + (rational r1, rational r2)
-    {
-        return rational(r1.num * r2.denom + r2.num*r1.denom, r1.denom * r2.denom) ;
-    }
-
-
-    
 
 };
+
+
+rational operator+(const rational& r1, const rational& r2)
+{
+    return rational(r1.num * r2.denom + r2.num * r1.denom, r1.denom * r2.denom) ;
+}
+
 
 
 int main()
@@ -38,7 +32,6 @@ int main()
     r1.print(); 
     r2.print(); 
     r3.print();
-    std :: cout << r3.eval()<< std :: endl;
     return 0;
 
 }
